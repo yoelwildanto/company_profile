8e60d52d14a58df01cbe9794babdba74125fa94c
yoelwildanto <yoel.wildantop@gmail.com>
2023-09-25 18:25:46 +0700

    up

--- main.js
+++ main.js
@@ -64,3 +64,22 @@ var swiper = new Swiper(".client-swiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a");
+
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
+
+navItems.forEach((navItem) =>{
+    navItem.addEventListener("click", () =>{
+        navigation.classList.remove("active");
+    })
+});
